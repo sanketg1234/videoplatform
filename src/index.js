d26d2610f0ce02dedbe835e5aa6e08d8677b763d
sanketg1234 <sanketghadge161205@gmail.com>
2025-05-17 09:33:02 +0530

    connected to database

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv'; 
+import connectDB  from './db/index.js';
+
+dotenv.config({
+    path: './env'
+});
+/*(async() =>{
+    try{
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`, )
+
+    }
+    catch(error){
+        console.log("error:",error)
+        throw error
+    }
+})() */
+
+
+
+
+    connectDB()
